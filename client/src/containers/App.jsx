01ff39a94366604c7adbdbc27bb9c9e43007624d
Travis Gee <travisgee@techtonics-MBP.hsd1.co.comcast.net>
2020-08-21 15:14:00 -0600

    Moves navbar to separate component

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
 import heroImage from '../assets/images/hero.jpg';
+import NavBar from './NavBar';
 
 export default class App extends Component {
   render() {
     return (
       <main>
-        <nav>
-          <a href="#">The Library</a>
-          <a href="#">Home</a>
-          <a href="#">Bookshelf</a>
-          <a href="#">Add Book</a>
-          <input type="text" placeholder="Search by Title/Author" />
-          <button>Search</button>
-        </nav>
+        <NavBar />
 
         <section>
           <h1>Books.</h1>
