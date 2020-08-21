5887016c96e16efe86b9cf18cdf1266dc9ac4c6f
Travis Gee <travisgee@techtonics-MBP.hsd1.co.comcast.net>
2020-08-21 10:45:38 -0600

    Adds initial index file

---
+++ client/src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const Application: React.FC<{}> = () => <h1>Application</h1>;
+
+render(<Application />, document.getElementById('root'));
